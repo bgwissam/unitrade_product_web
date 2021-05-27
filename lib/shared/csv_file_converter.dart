4a8e1c31412f378a6468898490d81dd47748f440
bgwissam <bgwissam@hotmail.com>
2021-05-27 07:28:15 +0300

    added database update for inventory

--- lib/shared/csv_file_converter.dart
+++ lib/shared/csv_file_converter.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:unitrade_web_v2/shared/loading.dart';
 
 class LoadCsvDataScreen extends StatelessWidget {
   final List<dynamic> file;
@@ -8,13 +10,21 @@ class LoadCsvDataScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('The map list: ${mapList[0]}');
+    bool _isUpdating = false;
     return Scaffold(
         appBar: AppBar(
           title: Text('CSV Data'),
           backgroundColor: Colors.amberAccent,
+          actions: [
+            TextButton(
+              onPressed: () async {
+                _updateDataPerItemCode();
+              },
+              child: Text('Update Data', style: TextStyle(color: Colors.black)),
+            )
+          ],
         ),
-        body: _buildDataDetails());
+        body: !_isUpdating ? _buildDataDetails() : Loading());
   }
 
   //Build data in the file as a Table view
@@ -62,5 +72,73 @@ class LoadCsvDataScreen extends StatelessWidget {
     }).toList();
     var flat = list.expand((i) => i).toList();
     return flat;
+  }
+
+  //Update stock level in the database
+  _updateDataPerItemCode() async {
+    String businessUnit;
+    List<String> missingCodes = [];
+
+    //loop over the maplist
+    for (var row in mapList) {
+      String businessLine = row['Business Line'];
+      String itemCode = row['Item Code'];
+
+      switch (businessLine) {
+        case '212003':
+          businessUnit = 'wood';
+          break;
+        case '212004':
+          businessUnit = 'paint';
+          break;
+        case '212005':
+          businessUnit = 'accessories';
+          break;
+        case '2120006':
+          businessUnit = 'solid';
+          break;
+      }
+      //get the item from the database
+      await FirebaseFirestore.instance
+          .collection(businessUnit)
+          .where('itemCode', isEqualTo: itemCode)
+          .get()
+          .then((value) {
+        if (value.docs.length == 0) {
+          //Will add the codes that are missing in our database to add them later
+          if (!missingCodes.contains(itemCode)) {
+            missingCodes.add(itemCode);
+          }
+        } else {
+          //_updateData(e.id, businessUnit, row['Inventory on Hand']);
+          var result = value.docs.map((e) {
+            return e;
+          });
+          result.forEach((element) {
+            print('Element: ${element.data().length}');
+            if (element.data().isNotEmpty || element.data() != null)
+              print('The result entities: ${element.data()}');
+          });
+          return result;
+        }
+      }).onError((error, stackTrace) {
+        print('An error occured with item ($itemCode): $error, $stackTrace');
+        return error;
+      }).catchError((error) {
+        print('An error occured obtained data for item: $itemCode');
+      });
+    }
+    print('The missing codes are: $missingCodes');
+  }
+
+  _updateData(String id, String businessUnit, int inventory) async {
+    await FirebaseFirestore.instance
+        .collection(businessUnit)
+        .doc(id)
+        .update({'inventoryOnHand': inventory})
+        .whenComplete(() => print('data updated properly'))
+        .onError((error, stackTrace) {
+          print('Could not update data due to: $error: $stackTrace');
+        });
   }
 }
